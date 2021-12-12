25390dee274afe4cb4f7690f242d82cd23859de9
rizalyoga <ach.rizal.yogaswara@gmail.com>
2021-12-12 20:23:30 +0700

    appJS

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import RegisterWO from "./views/weddingOrganizer/register/register-wo";
 import LoginWO from "./views/weddingOrganizer/login/login-wo";
 
 const App = () => {
-  return  <>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/vendor/register" element={<RegisterWO/>}></Route>
-                <Route path="/vendor/login" element={<LoginWO/>}></Route>
-              </Routes>
-            </BrowserRouter>
-          </>
-}
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/vendor/register" element={<RegisterWO />}></Route>
+          <Route path="/vendor/login" element={<LoginWO />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
