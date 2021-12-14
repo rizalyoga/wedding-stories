8803b5c037d9b091dfdcbfec2001147547960c54
AidaFna <93112554+AidaFna@users.noreply.github.com>
2021-12-14 21:39:21 +0700

    add Tambah Package Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import RegisterWO from "./views/weddingOrganizer/register/register-wo";
 import LoginWO from "./views/weddingOrganizer/login/login-wo";
 import DetailPackage from "./views/user/detail-package-user/detail-package.jsx";
 import ProfileWO from "./views/weddingOrganizer/profile/profile-wo";
+import ListPackage from "./views/weddingOrganizer/package/listPackage/listPackage";
+import FormAddPackage from "./views/weddingOrganizer/package/addPackage/formAddPackage";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path="/vendor/login" element={<LoginWO />}></Route>
         <Route path="/detail/package" element={<DetailPackage />}></Route>
         <Route path="/vendor/profile" element={<ProfileWO />}></Route>
+        <Route path="/vendor/packages" element={<ListPackage />}></Route>
+        <Route path="/vendor/packages/add" element={<FormAddPackage />}></Route>
       </Routes>
 
       <Footer />
--- src/views/components/navbar-wo/navbar-wo-login.jsx
+++ src/views/components/navbar-wo/navbar-wo-login.jsx
@@ -1,6 +1,7 @@
 import { Navbar, Container, Form, Button, NavDropdown } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import logoNavLogin from "../../../assets/virus.png";
+import swal from "sweetalert";
 import "./navbar-wo-login.css";
 
 const NavLoginWo = () => {
@@ -8,6 +9,10 @@ const NavLoginWo = () => {
 
   const logout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("id");
+    localStorage.removeItem("name");
+    localStorage.removeItem("role");
+    swal("You've Been Logged Out!");
     navigate("/");
   };
 
@@ -16,16 +21,45 @@ const NavLoginWo = () => {
       <Navbar style={{ background: "#fff" }}>
         <Container>
           <Navbar.Brand href="#home" className="logo">
-            <img onClick={() => navigate("/")} src={logoNavLogin} width="30" height="35" className="d-inline-block align-top" alt="logo" />
+            <img
+              onClick={() => navigate("/")}
+              src={logoNavLogin}
+              width="30"
+              height="35"
+              className="d-inline-block align-top"
+              alt="logo"
+            />
           </Navbar.Brand>
           <Form className="d-flex search-input">
-            <input type="text" style={{ borderRadius: "20px" }} className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <input
+              type="text"
+              style={{ borderRadius: "20px" }}
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
             {/* <Button class="btn btn-outline-success" type="submit"></Button> */}
           </Form>
-          <NavDropdown title={<i className="bi bi-person-circle" style={{ fontSize: 25, color: "#5C7893" }}></i>} id="collasible-nav-dropdown" className="menu-wo">
-            <NavDropdown.Item onClick={() => navigate("/")}>Home</NavDropdown.Item>
-            <NavDropdown.Item onClick={() => navigate("/vendor/profile")}>Profile</NavDropdown.Item>
-            <NavDropdown.Item>Packages</NavDropdown.Item>
+          <NavDropdown
+            title={
+              <i
+                className="bi bi-person-circle"
+                style={{ fontSize: 25, color: "#5C7893" }}
+              ></i>
+            }
+            id="collasible-nav-dropdown"
+            className="menu-wo"
+          >
+            <NavDropdown.Item onClick={() => navigate("/")}>
+              Home
+            </NavDropdown.Item>
+            <NavDropdown.Item onClick={() => navigate("/vendor/profile")}>
+              Profile
+            </NavDropdown.Item>
+            <NavDropdown.Item onClick={() => navigate("/vendor/packages")}>
+              Packages
+            </NavDropdown.Item>
             <NavDropdown.Item>List Order</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item onClick={() => logout()}>Logout</NavDropdown.Item>
